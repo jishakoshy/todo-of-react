bb5e151a44c10838faae9620fb3be84e16ad6ccb
jishakoshy <135108986+jishakoshy@users.noreply.github.com>
2024-01-29 14:55:52 +0530

    //

---
+++ todo/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React from 'react';
+import Todo from './Components/Todo';
+
+function App() {
+  return (
+    <div className='topContainer'>
+      <Todo />    
+    </div>
+  );
+}
+
+export default App;
